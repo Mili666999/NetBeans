2f0368b5092a6ab8cfee48512682a00c07b86f05
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-08-24 17:35:16 +0200

    Initial commit

---
+++ Edunova01/src/main/java/edunova/Start.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edunova;
+
+/**
+ *
+ * @author Mili
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        //new Prozor().setVisible(true);
+        new LjubavniKalkulator().setVisible(true);
+    }
+}
